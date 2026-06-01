3163596e0450a471be16bd0c9a50c75b919a7016
nadeem Al-aloosi <alaloosinadeem@gmail.com>
2026-06-01 18:22:47 +0200

    حلب البيانات من ملف جيسون انشاء صفحة السلع

--- zimam/src/views/productsView.vue
+++ zimam/src/views/productsView.vue
@@ -1,3 +1,67 @@
+<script setup lang="ts">
+import Basebutton from '@/components/Basebutton.vue';
+import { ref, onMounted } from 'vue'
+
+const products = ref([])
+const newProductName = ref('')
+const newProductPrice = ref(0)
+
+// 1. دالة الـ GET: لجلب المنتجات من السيرفر عند فتح الصفحة
+const fetchProducts = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/products')
+        products.value = await response.json()
+    } catch (error) {
+        console.error("فشل جلب البيانات:", error)
+    }
+}
+
+// تشغيل دالة الجلب تلقائياً بمجرد تحميل المكون في المتصفح
+onMounted(() => {
+    fetchProducts()
+})
+</script>
+
 <template>
-    Hallo World
+    <div class="flex justify-evenly mt-10">
+        <Basebutton>
+            <template #svg-img>
+                <img src="@/assets/svg/product.svg" alt="" srcset="">
+            </template>
+            <template #default>
+                سلعة جديدة
+            </template>
+        </Basebutton>
+
+        <Basebutton link="/">
+            <template #svg-img>
+                <img src="@/assets/svg/home.svg" alt="" srcset="">
+            </template>
+        </Basebutton>
+    </div>
+    <div class=" place-items-center mt-20 grid ">
+        <table class=" border-spacing-y-5 border-separate w-4/5 border-collapse ">
+            <thead class=" text-right border-b p-2 border-gray-200">
+                <tr>
+                    <th>تعديل</th>
+                    <th>السعر</th>
+                    <th>السلعة</th>
+                    <th>#</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="  text-right " v-for="product in products" :key="product.id">
+
+                    <td>edit</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.id }}</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+
 </template>
