07e048af0116c0ab198d8449439d77add292189e
nadeem Al-aloosi <alaloosinadeem@gmail.com>
2026-06-03 14:52:52 +0200

    فصل الاتصال بين الجدول والبيانات

--- zimam/src/components/ProductsTabel.vue
+++ zimam/src/components/ProductsTabel.vue
@@ -1,53 +1,35 @@
 <script setup>
-import { useProductStore } from '@/stores/useProductStore';
-import { ref, onMounted } from 'vue'
-import Basebutton from './Basebutton.vue';
-
-let products = useProductStore();
-// تشغيل دالة الجلب تلقائياً بمجرد تحميل المكون في المتصفح
-onMounted(() => {
-    products.fetchProducts()
-
+defineProps({
+    products: Object
 })
-function deleteById(id) {
-    products.deleteProductById(id)
-}
 </script>
 
 <template>
     <div class="grid place-items-center mt-20">
-        <table v-if="products.products.length > 0" class="w-4/5 text-right">
+        <table v-if="products.length > 0" class="w-4/5 text-right">
 
             <thead class="border-b border-gray-200 text-gray-500">
                 <tr>
                     <th class="py-4 px-2">#</th>
                     <th class="py-4 px-2">السلعة</th>
                     <th class="py-4 px-2">السعر</th>
+                    <slot newHeaders="headers"></slot>
                     <th class="py-4 px-2 text-center">إعدادات</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr v-for="(product, index) in products.products" :key="product.id"
+                <tr v-for="(product, index) in products" :key="product.id"
                     class="border-b border-gray-100 hover:bg-gray-50 transition-colors">
 
                     <td class="py-4 px-2">{{ index + 1 }}</td>
                     <td class="py-4 px-2 font-medium">{{ product.name }}</td>
                     <td class="py-4 px-2 text-blue-600 font-bold">{{ product.price }} دينار</td>
-
+                    <slot newItems="items"></slot>
                     <td class="py-4 px-2">
                         <div class="flex justify-evenly items-center gap-2">
-                            <Basebutton :link="/edit-product/ + product.id">
-                                <template #svg-img>
-                                    <img src="@/assets/svg/edit.svg" alt="">
-                                </template>
-                            </Basebutton>
+                            <slot name="setting-btn" :productId="product.id"></slot>
 
-                            <Basebutton @click.prevent="deleteById(product.id)">
-                                <template #svg-img>
-                                    <img src="@/assets/svg/delete-btn.svg" alt="">
-                                </template>
-                            </Basebutton>
                         </div>
                     </td>
 
--- zimam/src/views/productsView.vue
+++ zimam/src/views/productsView.vue
@@ -2,6 +2,19 @@
 import Basebutton from '@/components/Basebutton.vue';
 import ProductsTabel from '@/components/ProductsTabel.vue';
 import pagesheader from '@/components/pagesheader.vue';
+
+import { useProductStore } from '@/stores/useProductStore';
+import { ref, onMounted } from 'vue'
+
+let products = useProductStore();
+// تشغيل دالة الجلب تلقائياً بمجرد تحميل المكون في المتصفح
+onMounted(() => {
+    products.fetchProducts()
+
+})
+function deleteById(id) {
+    products.deleteProductById(id)
+}
 </script>
 
 <template>
@@ -19,7 +32,21 @@ import pagesheader from '@/components/pagesheader.vue';
         </template>
     </pagesheader>
 
-    <ProductsTabel />
+    <ProductsTabel :products="products.products">
+        <template #setting-btn="{ productId }">
+            <Basebutton :link="/edit-product/ + productId">
+                <template #svg-img>
+                    <img src="@/assets/svg/edit.svg" alt="">
+                </template>
+            </Basebutton>
+
+            <Basebutton @click.prevent="deleteById(productId)">
+                <template #svg-img>
+                    <img src="@/assets/svg/delete-btn.svg" alt="">
+                </template>
+            </Basebutton>
+        </template>
+    </ProductsTabel>
 
 
 </template>
